01b7dc18a021ccac804402c8939e0199e47fb4df
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-13 20:47:23 +0200

    :sparkles: add tic tav toe component

---
+++ src/components/TicTacToeApp.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import ButtonCompenent from './ButtonComponent.vue';
+import InputPlayer from './InputPlayer.vue'
+import type { Iplayer } from '../models/Player'
+import { ref } from 'vue'
+
+let player = localStorage.getItem('player')
+
+let clickValue = ref('');
+
+const startGame = (playerName:string) => {
+    console.log('starta spel', playerName)
+    //spara spelare till localStorage
+}
+</script>
+
+<template>
+<div class="content">
+    <InputPlayer @start-game="startGame" />
+</div>
+
+</template>
+
+<style scoped>
+.content {
+    position: absolute;
+    top: 150px;
+}
+</style>
---
+++ src/models/Player.ts
@@ -0,0 +1,6 @@
+export interface Iplayer {
+    name: string;
+    gamePiece: string;
+    avatar: string;
+
+}
