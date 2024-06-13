debfa173632721a57e294de560db7f0f9ba25ecb
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-13 19:36:35 +0200

    :sparkles: setup app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
+import TicTacToeApp from './components/TicTacToeApp.vue';
 
 </script>
 
 <template>
-
+    <header><h1>Tic Tac Toe</h1></header>
+  
+<TicTacToeApp />
 </template>
 
 <style scoped>
+header{
+   position: absolute;
+   top: 5px;
+}
+h1{
+    font-family: "Yatra One", system-ui;
 
+}
 </style>
