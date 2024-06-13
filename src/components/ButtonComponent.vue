45666c3daecf7836e4ad5291f0ca48f50dc203f9
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-13 20:46:57 +0200

    :sparkles: buttonComponent

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+interface IButtonProps {
+    buttonText: string;
+    buttonEvent: string;
+}
+
+const props = defineProps<IButtonProps>();
+
+const click = ref<IButtonProps>(props);
+
+const emit = defineEmits<{
+    (e: 'handleClick', value:string):void
+}>();
+
+</script>
+
+
+<template>
+<button @click="$emit('handleClick', click.buttonEvent) ">{{ buttonText }}</button>
+</template>
+
+<style scoped>
+button {
+    font-family: Raleway;
+    height: 40px;
+    width: 300px;
+    background-color: var(--color-darker-green);
+    border: none;
+    border-radius: 10px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-size: 0.7rem;
+    font-weight: 700;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    cursor: pointer;
+}
+button:hover{
+    background-color: var(--hover-color);
+}
+</style>
