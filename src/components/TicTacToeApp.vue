d89d41cb548184b75c23c5b53f4740c384895026
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-14 07:31:03 +0200

    :sparkles: checking players in localstorage

--- src/components/TicTacToeApp.vue
+++ src/components/TicTacToeApp.vue
@@ -1,23 +1,46 @@
 <script setup lang="ts">
 import ButtonCompenent from './ButtonComponent.vue';
 import InputPlayer from './InputPlayer.vue'
-import type { Iplayer } from '../models/Player'
+import type { IPlayer } from '../models/Player'
 import { ref } from 'vue'
 
-let player = localStorage.getItem('player')
+let players: IPlayer[] = []
+let enableGame = ref('enableGame');
 
-let clickValue = ref('');
-
-const startGame = (playerName:string) => {
-    console.log('starta spel', playerName)
-    //spara spelare till localStorage
+const init = () => {
+    //localStorage.clear()
+    const storedPlayers:IPlayer[] = JSON.parse(localStorage.getItem('players') || '{}')
+    if(storedPlayers.length === 2) {
+        //start Game
+        enableGame.value = '';
+        console.log(enableGame)
+    }
 }
+
+const startGame = (player: IPlayer) => {
+    console.log('starta spel', player)
+    players.push(player)
+    localStorage.setItem('players', JSON.stringify(players));
+
+    const storedPlayers: IPlayer[] = JSON.parse(localStorage.getItem('players') || '{}')
+    console.log('checka', storedPlayers.length)
+    if(storedPlayers.length === 2) {
+        enableGame.value = '';
+    }
+}
+
+
+init()
 </script>
 
 <template>
-<div class="content">
-    <InputPlayer @start-game="startGame" />
+<div class="content" v-if="enableGame === 'enableGame'">
+    <InputPlayer  @start-game="startGame" /> 
 </div>
+<div class="content"  v-if="enableGame === ''">
+    <div>{{ players }}</div>
+</div>
+
 
 </template>
 
@@ -26,4 +49,5 @@ const startGame = (playerName:string) => {
     position: absolute;
     top: 150px;
 }
+
 </style>
